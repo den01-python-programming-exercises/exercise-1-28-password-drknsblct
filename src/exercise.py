03608d449a235d99a4c08afdbcb8569dcb1131a8
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:46:40 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    p = input('Password?')
+     
+    if p == 'Caput Draconis':
+        print('Welcome!')
+    else:
+        print('Off with you!')
 
 if __name__ == '__main__':
     main()
